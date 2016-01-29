141cb69ad5f3ee4bb5366eb906991f20f50c28ac
shivakumar-p12 <shivakumar.p12@gmail.com>
2016-01-29 16:39:19 +0530

    Initial commit

---
+++ app/src/main/java/com/musicplayer2/mplayer2/MainActivity.java
@@ -0,0 +1,83 @@
+package com.musicplayer2.mplayer2;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    MediaPlayer mediaPlayer;
+    Button playpause;
+    boolean isPlaying = false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this,R.raw.barsoremegha);
+
+        playpause = (Button)findViewById(R.id.playpause);
+        playpause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isPlaying) {
+                    pause();
+                } else {
+                    play();
+                }
+                isPlaying = !isPlaying;
+            }
+        });
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+     /*   FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    public void play()
+    {
+        mediaPlayer.start();
+        playpause.setBackgroundResource(R.mipmap.playerpause);
+
+    }
+    public void pause()
+    {
+        mediaPlayer.pause();
+        playpause.setBackgroundResource(R.mipmap.playerplay);
+
+    }
+}
